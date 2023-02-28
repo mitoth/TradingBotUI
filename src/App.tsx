bbd6cb2ce16402ff10eb989bf4d8c65b5236d6ed
Mihai Toth <toth.mihai@yahoo.com>
2023-02-28 22:01:09 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button type="button" className="btn btn-danger">
+          Danger
+        </button>
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
